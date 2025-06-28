e9835e162615774bcc53ab8066de4d425a36ce0c
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-06-28 20:07:32 +0530

    Create LogFragment.kt

---
+++ app/src/main/java/com/example/catprepapp/LogFragment.kt
@@ -0,0 +1,16 @@
+package com.example.catprepapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LogFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }
+}
