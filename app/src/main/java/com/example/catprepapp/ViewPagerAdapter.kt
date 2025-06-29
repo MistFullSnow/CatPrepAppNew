2772955667a73ec6802b23b6210d5d4c328b2e66
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-06-29 17:51:47 +0530

    Update ViewPagerAdapter.kt

--- app/src/main/java/com/example/catprepapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/catprepapp/ViewPagerAdapter.kt
@@ -7,14 +7,15 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
 
-    override fun getItemCount(): Int = 4
+    override fun getItemCount(): Int = 5
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> DashboardFragment() // Was ScheduleFragment
-            1 -> ScheduleFragment() // Was LogFragment
-            2 -> LogFragment()      // Was DashboardFragment
-            3 -> CatBotFragment()   // Stays the same
+            1 -> InboxFragment()
+            2 -> ScheduleFragment() // Was LogFragment
+            3 -> LogFragment()      // Was DashboardFragment
+            4 -> CatBotFragment()   // Stays the same
             else -> DashboardFragment()
         }
     }
