8dbff57420fd3485b03b3c802213e84a075c6cf0
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-06-29 16:15:12 +0530

    Create MyFirebaseMessagingService.kt

---
+++ app/src/main/java/com/example/catprepapp/MyFirebaseMessagingService.kt
@@ -0,0 +1,24 @@
+package com.example.catprepapp
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d("FCM", "From: ${remoteMessage.from}")
+
+        // This is where we will later build and show a notification
+        remoteMessage.notification?.let {
+            Log.d("FCM", "Message Notification Body: ${it.body}")
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("FCM", "Refreshed token: $token")
+        // Here you would send the token to your server to store it
+    }
+}
